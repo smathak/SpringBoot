9c0b61c9a553b1060613f320b37668a83c9bcdd1
smathak <poiujkl9875@hanmail.net>
2019-05-18 13:08:41 +0900

    This is first sprinb boot app

---
+++ src/main/java/springbootstarter/hello/HelloController.java
@@ -0,0 +1,13 @@
+package springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String printHello() {
+		return "Hello Spring Boot"; 
+	}
+}
